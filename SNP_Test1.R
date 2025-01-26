6da9b79544ad50dcfd391a29aa594642620f93cb
Ekaterina Bocharova <116888495+EkaterinaBocharova@users.noreply.github.com>
2025-01-27 00:40:07 +0300

    Update SNP_Test1.R

--- SNP_Test1.R
+++ SNP_Test1.R
@@ -17,21 +17,21 @@ gl
 
 # Расчет эвклидовых генетических дистанций
 distgenEUCL <- dist(gl, method = "euclidean", diag = FALSE, upper = FALSE, p = 2)
-hist(distgenEUCL)
 distgenEUCL
-
-# Визуализация PCA
-pca1 <- glPca(gl)
-pca1
-myCol <- colorplot(pca1$scores,pca1$scores, transp=TRUE, cex=1)
-abline(h=0,v=0, col="grey")
-add.scatter.eig(pca1$eig[1:40],2,1,2, posi="topleft", inset=.05, ratio=.3)
+min(distgenEUCL)
+max(distgenEUCL)
 
 # Визуализация NJ
 tre <- nj(dist(as.matrix(gl)))
-plot(tre, show.tip=FALSE)
-tiplabels(pch=20, col=myCol, cex=1)
+plot(tre, cex=0.4)
 title("NJ tree")
+
+# Визуализация PCA
+pca1 <- glPca(gl)
+myCol <- colorplot(pca1$scores,pca1$scores, transp=TRUE, cex=1)
+abline(h=0,v=0, col="grey")
+add.scatter.eig(pca1$eig[1:40],2,1,2, posi="topleft", inset=.05, ratio=.3)
+title("PCA")
 
 
 
